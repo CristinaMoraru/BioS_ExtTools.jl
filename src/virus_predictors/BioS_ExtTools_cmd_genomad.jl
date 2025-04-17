f25c832df07e28970befe6182b1fa7444d0c0c4f
CristinaMoraru <lilianacristina.moraru@uni-due.de>
2025-04-17 10:34:40 +0200

    v0.4.4

--- src/virus_predictors/BioS_ExtTools_cmd_genomad.jl
+++ src/virus_predictors/BioS_ExtTools_cmd_genomad.jl
@@ -9,8 +9,28 @@ struct RunGenomadCmd <: BioinfCmd
     input_f::FnaP
     output_d::String
     database::String
-    min_score::Float64
+    min_score::Union{Float64, Nothing}
+    num_threads::Int64
+    other_options::String
 end
 
-build_cmd(obj::RunGenomadCmd) = `genomad $(obj.task) --cleanup --splits 8 $(obj.input_f.p) $(obj.output_d) $(obj.database) --min-score $(obj.min_score)`
-build_cmd(obj::RunGenomadCmd, parentD::String) = `genomad $(obj.task) --cleanup --splits 8 $parentD/$(obj.input_f.p) $parentD/$(obj.output_d) $(obj.database) --min-score $(obj.min_score)`
+function build_cmd(obj::RunGenomadCmd) 
+    if isnothing(obj.min_score)
+        cmd = `genomad $(obj.task) --cleanup --splits 8 $(obj.input_f.p) $(obj.output_d) $(obj.database) --threads $(obj.num_threads) $(obj.other_options)`
+    else
+        cmd = `genomad $(obj.task) --cleanup --splits 8 $(obj.input_f.p) $(obj.output_d) $(obj.database) --threads $(obj.num_threads) --min-score $(obj.min_score) $(obj.other_options)`
+    end
+
+    return cmd
+end
+
+function build_cmd(obj::RunGenomadCmd, parentD::String) 
+    if isnothing(obj.min_score)
+        cmd = `genomad $(obj.task) --cleanup --splits 8 $parentD/$(obj.input_f.p) $parentD/$(obj.output_d) $(obj.database) --threads $(obj.num_threads) $(obj.other_options)`
+    else
+        cmd = `genomad $(obj.task) --cleanup --splits 8 $parentD/$(obj.input_f.p) $parentD/$(obj.output_d) $(obj.database) --threads $(obj.num_threads) --min-score $(obj.min_score) $(obj.other_options)`
+    end
+
+    return cmd
+end
+
